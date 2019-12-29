8b03bf1c91c4b36a626c17b5c645bd94529cec7c
gitdemo <long331456@163.com>
2019-12-29 20:34:47 +0800

    test

---
+++ src/main/java/com/offcn/Demo.java
@@ -0,0 +1,21 @@
+package com.offcn;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+/**
+ * All rights Reserved, Designed By www.info4z.club
+ * <p>title:com.offcn</p>
+ * <p>ClassName:Demo</p>
+ * <p>Description:TODO(请用一句话描述这个类的作用)</p>
+ * <p>Compony:Info4z</p>
+ * author:noauthor
+ * date:2019/12/29
+ * version:1.0
+ * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+}
